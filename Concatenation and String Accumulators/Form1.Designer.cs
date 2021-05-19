0b59dbde9289f03b3adc249bd6d9b6234818d5dc
Jikrebello <33652252+Jikrebello@users.noreply.github.com>
2021-05-19 23:22:19 +0200

    Add files via upload

---
+++ Concatenation and String Accumulators/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿
+namespace Concatenation_and_String_Accumulators
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtDisplay = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtDisplay
+            // 
+            this.txtDisplay.Location = new System.Drawing.Point(12, 12);
+            this.txtDisplay.Multiline = true;
+            this.txtDisplay.Name = "txtDisplay";
+            this.txtDisplay.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtDisplay.Size = new System.Drawing.Size(543, 80);
+            this.txtDisplay.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 146);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(543, 30);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Question 1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 250);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(543, 30);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Question 2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 371);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(543, 30);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Question 3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 468);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(543, 30);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Question 4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 561);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(543, 30);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Question 5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(12, 654);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(543, 30);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Question 6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(543, 48);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Given a number, print it out forwards and backwards, then add them together. Eg. " +
+    "234 + 432 = 666.";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(12, 191);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(543, 56);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Enter two six digit numbers.From each number, form a new number consisting of the" +
+    " 2nd, 4th, and 6th digits.Find the sum of these new formed numbers.";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(12, 296);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(543, 72);
+            this.label3.TabIndex = 10;
+            this.label3.Text = resources.GetString("label3.Text");
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(9, 420);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(546, 45);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Find all 3 digit whole numbers that are divisible by the product of their digits." +
+    "There are 20 such numbers.";
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(9, 513);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(546, 45);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "For numbers from 1000-2000 determine which are divisible by the sum of the square" +
+    "s of their digits.";
+            // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(9, 606);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(546, 45);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Take a 3 digits numbers like 200, reverse it(002), and then multiply the two numb" +
+    "ers. The result 400 is a perfect square. Find all such 3 digit numbers between 1" +
+    "00-300.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(575, 698);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtDisplay);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtDisplay;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Concatenation and String Accumulators/Form1.cs
@@ -0,0 +1,174 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Windows.Forms;
+
+namespace Concatenation_and_String_Accumulators
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //1. Given a number, print it out forwards and backwards, then add them together.Eg. 234 + 432 = 666.
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtDisplay.Text = "";
+            string value = Interaction.InputBox("Enter a number", "Reverse string adder", "");
+            string reverseValue = "";
+
+            for (int i = value.Length - 1; i >= 0; i--)
+            {
+                char reversedChar = Convert.ToChar(value.Substring(i, 1));
+                reverseValue += reversedChar;
+            }
+
+            int sum = Convert.ToInt32(value) + Convert.ToInt32(reverseValue);
+            string displayValues = value + " + " + reverseValue + " = " + sum;
+            txtDisplay.Text = displayValues;
+
+        }
+
+        //2. Enter two six digit numbers.From each number, form a new number consisting of the 2nd, 4th, and 6th digits.Find the sum of these new formed numbers.
+        //Example: 623841 -> 281, 711846 -> 186, Sum -> 467
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtDisplay.Text = "";
+            string value1 = "";
+            while (value1.Length != 6)
+            {
+                value1 = Interaction.InputBox("Enter the first 6 digit number", "Reverse string adder", "");
+                if (value1.Length == 6)
+                    break;
+                MessageBox.Show("Please enter a 6 digit number");
+            }
+            string splitValue1 = value1.Substring(1, 1);
+            splitValue1 += value1.Substring(3, 1);
+            splitValue1 += value1.Substring(5, 1);
+
+            string value2 = "";
+            while (value2.Length != 6)
+            {
+                MessageBox.Show("Please enter a 6 digit number");
+                value2 = Interaction.InputBox("Enter the first 6 digit number", "Reverse string adder", "");
+            }
+            string splitValue2 = value2.Substring(1, 1);
+            splitValue2 += value2.Substring(3, 1);
+            splitValue2 += value2.Substring(5, 1);
+
+            int sum = Convert.ToInt32(splitValue1) + Convert.ToInt32(splitValue2);
+            string displayValues = splitValue1 + " + " + splitValue2 + " = " + sum;
+            txtDisplay.Text = displayValues;
+        }
+
+        //3. Consider that a drivers registration number has the format 94807856590702. Write a program which checks the validity of this number.
+        //If the number is valid, the digits when totalled must be divisible evenly by 7.
+        private void button3_Click(object sender, EventArgs e)
+        {
+            txtDisplay.Text = "";
+            string value = "";
+            while (value.Length != 14)
+            {
+                value = Interaction.InputBox("Enter your ID number", "ID number Checker", "");
+                if (value.Length == 14)
+                    break;
+                else
+                    MessageBox.Show("Please check that the ID number is the correct length");
+            }
+
+            int valueSum = 0;
+            for (int i = 0; i < value.Length; i++)
+                valueSum += Convert.ToInt32(value[i]);
+
+            valueSum %= 7;
+
+            if (valueSum == 0)
+            {
+                MessageBox.Show("Valid ID Number");
+                string displayValues = value + " % 7 = " + valueSum;
+                txtDisplay.Text = displayValues;
+            }
+            else
+                MessageBox.Show("Invalid ID Number");
+
+        }
+
+        //4. Find all 3 digit whole numbers that are divisible by the product of their digits.There are 20 such numbers.
+        private void button4_Click(object sender, EventArgs e)
+        {
+            txtDisplay.Text = "";
+
+            for (int number = 100; number <= 999 ; number++) // for the first 3 (100) digit number and the last 3 digit number (999)
+            {
+                int product = 1;
+                string num = number.ToString();
+
+                for (int digit = 0; digit < num.Length; digit++)
+                    product *= Convert.ToInt32(num.Substring(digit, 1));
+                
+                if (product != 0)
+                {
+                    if (number % product == 0)
+                    {
+                        txtDisplay.Text += number + "\tis divisible by the product of its digits\t" + product + Environment.NewLine;  
+                    }
+                }
+            }
+        }
+
+        //5. For numbers from 1000-2000 determine which are divisible by the sum of the squares of their digits.
+        private void button5_Click(object sender, EventArgs e)
+        {
+            txtDisplay.Text = "";
+
+            for (int number = 1000; number <= 2000; number++)
+            {
+                int sum = 0;
+                string num = number.ToString();
+
+                for (int digit = 0; digit < num.Length; digit++)
+                {
+                    double square = Math.Pow(Convert.ToDouble(num.Substring(digit, 1)) , 2);
+                    sum += Convert.ToInt32(square);
+                }
+
+                if (sum > 0)
+                {
+                    if (number % sum == 0)
+                    {
+                        txtDisplay.Text += number + "\tis divisible by the sum of the squares of its digits\t" + sum + Environment.NewLine;
+                    }
+                }
+            }
+        }
+
+        //6. Take a 3 digits numbers like 200, reverse it(002), and then multiply the two numbers.
+        //The result 400 is a perfect square. Find all such 3 digit numbers between 100-300.
+        private void button6_Click(object sender, EventArgs e)
+        {
+            txtDisplay.Text = "";
+
+            for (int number = 100; number <= 300; number++)
+            {
+                string num = number.ToString();
+                string reversedNum = "";
+
+                for (int digit = num.Length - 1; digit >= 0; digit--)
+                {
+                    char reversedChar = Convert.ToChar(num.Substring(digit, 1));
+                    reversedNum += reversedChar;
+                }
+
+                int product = number * Convert.ToInt32(reversedNum);
+                double square = Convert.ToDouble(Math.Sqrt(product));
+
+                if (square % 1 == 0)
+                {
+                    txtDisplay.Text += "Original Number: " + number + "\t\tReversed Number: " + reversedNum + Environment.NewLine + "Product of the Original Number and the Reversed Number: " + product + Environment.NewLine + "The square-root of the Product: " + square + Environment.NewLine;
+                }
+
+            }
+        }
+    }
+}
